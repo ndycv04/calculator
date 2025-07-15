ece45e0123c0ac21587c458796848fc1c8335175
ndycv04 <1809nadiah@gmail.com>
2025-07-15 15:46:15 +0800

    creating the functions

---
+++ script.js
@@ -0,0 +1,15 @@
+const add = function(a, b){
+  a + b;
+};
+
+const subtract = function(a, b){
+  a - b;
+};
+
+const multiply = function(a, b){
+  a * b;
+};
+
+const divide = function(a, b){
+  a / b;
+};
